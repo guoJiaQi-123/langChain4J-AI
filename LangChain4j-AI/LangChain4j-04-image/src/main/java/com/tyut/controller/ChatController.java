4962adbb0ab2d3748942b74d1a65a322b5bef69b
GuoJiaqi <guojiaqi5946@outlook.com>
2024-12-07 20:06:57 +0800

    「更新」LangChain4j视觉理解

--- LangChain4j-AI/LangChain4j-04-image/src/main/java/com/tyut/controller/ChatController.java
+++ LangChain4j-AI/LangChain4j-04-image/src/main/java/com/tyut/controller/ChatController.java
@@ -1,18 +1,15 @@
 package com.tyut.controller;
 
 
-import com.tyut.service.IServiceAssistant;
 import dev.langchain4j.data.message.ImageContent;
 import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,15 +25,20 @@ import java.util.Base64;
 public class ChatController {
 
     @Autowired
-    private IServiceAssistant iServiceAssistant;
-    @Autowired
     private ChatLanguageModel chatLanguageModel;
 
+    /**
+     * AI视觉理解
+     * @param file 上传的图片
+     * @param message 理解的方向
+     */
     @PostMapping("/image")
     public String image(MultipartFile file, String message) throws IOException {
+        // 获得图片的base64编码
         InputStream inputStream = file.getInputStream();
         byte[] bytes = inputStream.readAllBytes();
         String base64 = Base64.getEncoder().encodeToString(bytes);
+        // 构造用户数据
         UserMessage userMessage =
                 UserMessage.from(
                         TextContent.from(message),
